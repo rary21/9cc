fbfd7619300cfa9056eea5d83d24ab815384c2ef
rary21 <s1413091@gmail.com>
2019-09-25 23:57:11 +0900

    [add] new test case for arithmetic operatons (no parenteses)

--- test.sh
+++ test.sh
@@ -16,7 +16,14 @@ try() {
   fi
 }
 
+# add and sub
 try 0 "20 +  5-  25"
 try 24 "12  -24  + 36"
+# mul and div
+try 13 "13 *4 / 4"
+try 6 "52/ 26 *  3"
+# all arithmetic calculation
+try 21 "78 - 34 * 2 + 22/2"
+try 43 "21*2 + 24/6 - 3"
 
 echo OK
